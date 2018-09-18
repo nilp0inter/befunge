14c4ebb1d5db07a55089e55b0fb5e073537127b2
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-09-18 17:51:16 +0200

    Random operator.

--- Python/compiler/compiler.py
+++ Python/compiler/compiler.py
@@ -3,6 +3,7 @@ from itertools import zip_longest
 from pprint import pprint
 import dataclasses
 import enum
+import pickle
 import sys
 import typing
 
@@ -70,7 +71,9 @@ class Grid(dict):
     def __init__(self, s, height=HEIGHT, width=WIDTH):
         self.height = height
         self.width = width
-        for row, y in zip_longest(s.splitlines(), range(height), fillvalue=None):
+        for row, y in zip_longest(s.splitlines(),
+                                  range(height),
+                                  fillvalue=None):
             if row is None:
                 row = ""
             for char, x in zip_longest(row, range(width), fillvalue=None):
@@ -121,7 +124,7 @@ class CodeTree:
                 direction=cell.direction,  # same direction
                 x=x,
                 y=y,
-                stringmode=cell.content != '"',  # toggle on \"
+                stringmode=cell.content != '"',
                 content=c)
         elif cell.content == '"':
             x, y, c = self.grid.nextto(cell.x, cell.y, cell.direction)
@@ -129,7 +132,7 @@ class CodeTree:
                 direction=cell.direction if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=True,  # toggle on \"
+                stringmode=True,
                 content=c)
         elif Direction.valid(cell.content):
             newdirection = Direction(cell.content)
@@ -138,7 +141,7 @@ class CodeTree:
                 direction=newdirection if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
         elif cell.content == '_':
             x, y, c = self.grid.nextto(cell.x, cell.y, Direction.LEFT)
@@ -146,14 +149,14 @@ class CodeTree:
                 direction=Direction.LEFT if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             x, y, c = self.grid.nextto(cell.x, cell.y, Direction.RIGHT)
             right = CELL(
                 direction=Direction.RIGHT if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             return BRANCH(left=left, right=right)
         elif cell.content == '|':
@@ -162,14 +165,14 @@ class CodeTree:
                 direction=Direction.UP if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             x, y, c = self.grid.nextto(cell.x, cell.y, Direction.DOWN)
             down = CELL(
                 direction=Direction.DOWN if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             return BRANCH(up=up, down=down)
         elif cell.content == '?':
@@ -178,28 +181,28 @@ class CodeTree:
                 direction=Direction.UP if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             x, y, c = self.grid.nextto(cell.x, cell.y, Direction.DOWN)
             down = CELL(
                 direction=Direction.DOWN if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             x, y, c = self.grid.nextto(cell.x, cell.y, Direction.LEFT)
             left = CELL(
                 direction=Direction.LEFT if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             x, y, c = self.grid.nextto(cell.x, cell.y, Direction.RIGHT)
             right = CELL(
                 direction=Direction.RIGHT if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
             return BRANCH(up=up, right=right, down=down, left=left)
         elif cell.content == '#':
@@ -209,7 +212,7 @@ class CodeTree:
                 direction=cell.direction if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
         elif cell.content == '@':
             return None
@@ -219,7 +222,7 @@ class CodeTree:
                 direction=cell.direction if c != '@' else Direction.WHATEVER,
                 x=x,
                 y=y,
-                stringmode=False,  # toggle on \"
+                stringmode=False,
                 content=c)
 
     def _walk(self, cell):
@@ -250,9 +253,15 @@ ft = ir.FunctionType(i64, ())
 
 
 class LLVMBuilder:
-    def __init__(self, tree, name):
+    def __init__(self, tree, name, stack_size=1000, safe_stack=True):
         self.blocks = dict()
         self.tree = tree
+        if safe_stack:
+            self._pop_to = self._safe_pop_to
+            self._peek_to = self._safe_peek_to
+        else:
+            self._pop_to = self._unsafe_pop_to
+            self._peek_to = self._unsafe_peek_to
 
         self.module = ir.Module(name=name)
 
@@ -261,17 +270,36 @@ class LLVMBuilder:
             self.module,
             ir.FunctionType(ir.IntType(64), [ir.IntType(64)]),
             'putchar')
+        self._f_srandom = ir.Function(
+            self.module,
+            ir.FunctionType(ir.VoidType(), [ir.IntType(64)]),
+            'srandom')
+        self._f_random = ir.Function(
+            self.module,
+            ir.FunctionType(ir.IntType(64), []),
+            'random')
+        self._f_time = ir.Function(
+            self.module,
+            ir.FunctionType(ir.IntType(64), [ir.IntType(64).as_pointer()]),
+            'time')
 
         main = ir.Function(self.module, ft, 'main')
         builder = self._add_block(main, 'code')
 
         # Global variables
-        self._v_stack_ptr = builder.alloca(stacktype, size=1000, name='stack')
+        self._v_stack_ptr = builder.alloca(stacktype, size=stack_size,
+                                           name='stack')
         self._v_sp_ptr = builder.alloca(i64, name='sp')
         builder.store(i64(0), self._v_sp_ptr)
         self._v_a_ptr = builder.alloca(i64, name='a')
         self._v_b_ptr = builder.alloca(i64, name='b')
         self._v_c_ptr = builder.alloca(i64, name='x')
+
+        # Init PRNG
+        builder.call(self._f_srandom,
+                     [builder.call(
+                         self._f_time,
+                         [ir.Constant(ir.IntType(64).as_pointer(), 'null')])])
 
         self._build_branch(builder, tree.tree)
 
@@ -286,7 +314,7 @@ class LLVMBuilder:
         builder = ir.IRBuilder(block)
         return builder
 
-    def _pop_to(self, builder, dst_ptr=None):
+    def _safe_pop_to(self, builder, dst_ptr=None):
         sp = builder.load(self._v_sp_ptr)
         prep = builder.icmp_unsigned('==', sp, i64(0))
         with builder.if_else(prep) as (then, otherwise):
@@ -300,7 +328,15 @@ class LLVMBuilder:
                 builder.store(elem, dst_ptr)
                 builder.store(sp, self._v_sp_ptr)
 
-    def _peek_to(self, builder, dst_ptr):
+    def _unsafe_pop_to(self, builder, dst_ptr=None):
+        sp = builder.load(self._v_sp_ptr)
+        sp = builder.sub(sp, i64(1))
+        elem_addr = builder.gep(self._v_stack_ptr, [sp])
+        elem = builder.load(builder.bitcast(elem_addr, i64.as_pointer()))
+        builder.store(elem, dst_ptr)
+        builder.store(sp, self._v_sp_ptr)
+
+    def _safe_peek_to(self, builder, dst_ptr):
         sp = builder.load(self._v_sp_ptr)
         prep = builder.icmp_unsigned('==', sp, i64(0))
         with builder.if_else(prep) as (then, otherwise):
@@ -311,6 +347,13 @@ class LLVMBuilder:
                 elem_addr = builder.gep(self._v_stack_ptr, [sp])
                 elem = builder.load(builder.bitcast(elem_addr, i64.as_pointer()))
                 builder.store(elem, dst_ptr)
+
+    def _unsafe_peek_to(self, builder, dst_ptr):
+        sp = builder.load(self._v_sp_ptr)
+        sp = builder.sub(sp, i64(1))
+        elem_addr = builder.gep(self._v_stack_ptr, [sp])
+        elem = builder.load(builder.bitcast(elem_addr, i64.as_pointer()))
+        builder.store(elem, dst_ptr)
 
     def _push_from(self, builder, src_ptr):
         sp = builder.load(self._v_sp_ptr)
@@ -373,12 +416,12 @@ class LLVMBuilder:
                 with otherwise:
                     self._push_value(builder, i64(0))
         elif cell.content == '&':
-            pass
+            raise NotImplemented
         elif cell.content == '~':
-            pass
+            raise NotImplemented
         elif cell.content == '.':
-            pass
-        elif cell.content == ',':
+            raise NotImplemented
+        elif cell.content in ',':
             self._pop_to(builder, self._v_a_ptr)
             a = builder.load(self._v_a_ptr)
             builder.call(self._f_putchar, [a])
@@ -408,12 +451,16 @@ class LLVMBuilder:
                 then_code = cell.next.up
                 else_code = cell.next.down
 
-            prep = builder.icmp_unsigned('==', i64(0), i64(0))
+            self._pop_to(builder, self._v_a_ptr)
+            a = builder.load(self._v_a_ptr)
+
+            prep = builder.icmp_unsigned('!=', i64(0), a)
             with builder.if_else(prep) as (then_block, else_block):
                 with then_block:
                     self._build_branch(builder, then_code)
                 with else_block:
                     self._build_branch(builder, else_code)
+            builder.unreachable()
         elif cell.content == '?':
             up = self._add_block(builder, f"{cell.label}_UP")
             self._build_branch(up, cell.next.up)
@@ -424,7 +471,10 @@ class LLVMBuilder:
             right = self._add_block(builder, f"{cell.label}_RIGHT")
             self._build_branch(right, cell.next.right)
 
-            switch = builder.switch(i64(0), right.block)
+            a = builder.call(self._f_random, [])
+
+            a = builder.srem(a, i64(4))  # TODO: prove no bias
+            switch = builder.switch(a, right.block)
             switch.add_case(i64(0), up.block)
             switch.add_case(i64(1), down.block)
             switch.add_case(i64(2), left.block)
@@ -435,13 +485,11 @@ class LLVMBuilder:
                 builder.ret(i64(0))
             except AssertionError:
                 # Closed already?
-                # print(f"CLOSED {cell.x} {cell.y}")
-                pass
+                print(f"; CLOSED {cell.x} {cell.y}")
         elif cell.content in 'pg':
             raise ValueError('p & g are not allowed')
         else:
-            # print("Skipping %r" % cell.content)
-            pass
+            print("; Skipping %r" % cell.content)
 
     def _build_branch(self, builder, tree):
         if isinstance(tree, CODE):
@@ -451,8 +499,7 @@ class LLVMBuilder:
                     builder.branch(self.blocks[tree.cell.label])  # Implicit jump
                 except AssertionError:
                     # Closed already?
-                    # print(f"CLOSED {tree.cell.x} {tree.cell.y}")
-                    pass
+                    print(f"; CLOSED {tree.cell.x} {tree.cell.y}")
                 builder = newbuilder
             self._build_instruction(builder, tree.cell)
             if not isinstance(tree.cell.next, BRANCH):
@@ -474,7 +521,7 @@ if __name__ == '__main__':
         code = fp.read()
         grid = Grid(code)
         tree = CodeTree(grid)
-        # pprint(tree.tree)
-        # pprint(tree.destinations)
-        builder = LLVMBuilder(tree, 'mymodule')
+        # with open(sys.argv[1] + '.tree', 'wb') as tdump:
+        #     pickle.dump(tree.tree, tdump)
+        builder = LLVMBuilder(tree, 'mymodule', safe_stack=False, stack_size=3)
         print(builder.module)
